dcc44a5ac39b2a21017c27ea313af6f59bdd1744
fedorov0av <fedorov0av@gmail.com>
2025-02-24 23:05:19 +0300

    changed vite template

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import viteLogo from "./assets/ts.svg";
+import "./App.css";
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  return (
+    <>
+      <div>
+        <a target="_blank">
+          <img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+      </div>
+      <h1>TSRepoSearch</h1>
+      <div className="card">
+        <p>Введите название репозитория и нажмите Enter</p>
+        <input
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        ></input>
+        <p>{firstName}</p>
+      </div>
+      <p className="read-the-docs">
+        Поиск репозиториев на GitHub не был никогда таким легким, как сейчас...
+      </p>
+    </>
+  );
+}
+
+export default App;
